4a752df8c7b1b27f762e8ad5b103549cc25c9971
John Haiducek <jhaiduce@gmail.com>
2021-06-25 13:06:35 -0400

    Fix compiler errors

--- callbacks.f90
+++ callbacks.f90
@@ -1,42 +1,53 @@
 module callbacks
 
-  use iso_c_binding, ONLY: c_double, c_int
+  use iso_c_binding, ONLY: c_double, c_int, c_funptr, c_f_procpointer
   implicit none
 
   ! C-compatible type to store a pointer to a callback procedure
   type, bind(c)::mgr
-     procedure(callback_signature), pointer::callback=>null()
+     type(c_funptr)::callback
   end type mgr
 
   ! Declare signature of callback
   abstract interface
      subroutine callback_signature(size,arr)
+
        ! Callback function that modifies an array
+
+       use iso_c_binding, ONLY: c_double, c_int
+
        integer(c_int), value::size
            !! Size of array
+
        real(c_double)::arr
            !! Array data
+
      end subroutine callback_signature
   end interface
 
 contains
 
-  subroutine register_callback(mgr, callback) bind(c)
+  subroutine register_callback(manager, callback) bind(c)
 
     ! Register a callback by storing it in mgr
 
-    type(mgr), pointer::mgr
-    procedure(callback_signature), pointer::callback
+    type(mgr), intent(inout), pointer::manager
+    type(c_funptr), intent(in), value::callback
 
-    ! Store the callback (Note that we could just call the callback from here
-    ! directly instead of storing it)
-    mgr%callback=callback
+    manager%callback=callback
 
   end subroutine register_callback
 
-  subroutine use_callback(mgr,size,arr) bind(c)
+  subroutine use_callback(manager,size,arr) bind(c)
 
-    call mgr%callback(size,arr)
+    type(mgr), pointer::manager
+    integer(c_int)::size
+    real(c_double)::arr
+    procedure(callback_signature), pointer::callback
+
+    call c_f_procpointer(manager%callback, callback)
+
+    call callback(size,arr)
 
   end subroutine use_callback
 
